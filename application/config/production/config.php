08a3c049418144c6f589209560a7e12c7cdf86de
John Arley Cano <johnarleycano@hotmail.com>
2019-10-18 16:38:52 -0400

    refactor(General): cambios generales)

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://entreamigosdecolombia.org/';
+$config['base_url']	= 'http://entreamigosdecolombia.org';
 
 /*
 |--------------------------------------------------------------------------
